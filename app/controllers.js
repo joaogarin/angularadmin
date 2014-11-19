c17750bafafa03acb69d68871e6fde133c1535d5
joaogarin <joao.garin@gmail.com>
2014-11-19 14:58:49 +0100

    Fixes on themes layout

--- app/controllers.js
+++ app/controllers.js
@@ -1,6 +1,7 @@
-/**************************
+/*
  Application controllers
- **************************/
+ Main controllers for the app
+ */
 
 angular.module("app.controllers", []).controller("AdminAppCtrl", ["$scope", "$location",
         function($scope, $location) {
@@ -35,7 +36,7 @@ angular.module("app.controllers", []).controller("AdminAppCtrl", ["$scope", "$lo
 
 
 
-/**************************
+/*
 
  Chart controllers
 
@@ -46,7 +47,7 @@ angular.module("app.controllers", []).controller("AdminAppCtrl", ["$scope", "$lo
  FlotCharts
  http://omnipotent.net/jquery.sparkline/ - Sparkline
 
- **************************/
+*/
 
 angular.module("app.chart.ctrls", []).controller("chartingCtrl", ["$scope",
         function($scope) {
@@ -859,9 +860,10 @@ angular.module("app.chart.ctrls", []).controller("chartingCtrl", ["$scope",
 
 
 
-/**************************
+/*
  App tasks controllers
- **************************/
+ Main task controllers (includes saving tasks into localStorage)
+ */
 
 angular.module("app.task", []).factory("taskStorage", function() {
 
@@ -943,9 +945,10 @@ angular.module("app.task", []).factory("taskStorage", function() {
 
 
 
-/**************************
+/*
  App Form validations
- **************************/
+ Validator functions for form elements (signIn,signUp and custom forms)
+ */
 
 angular.module("app.form.validation", []).controller("wizardFormCtrl", ["$scope",
         function($scope) {
@@ -1044,9 +1047,10 @@ angular.module("app.form.validation", []).controller("wizardFormCtrl", ["$scope"
 
 
 
-/**************************
+/*
  App Form Ui Controls
- **************************/
+ Controllers for form Ui components
+ */
 
 angular.module("app.ui.form.ctrls", []).controller("TagsDemoCtrl", ["$scope",
         function($scope) {
@@ -1117,9 +1121,10 @@ angular.module("app.ui.form.ctrls", []).controller("TagsDemoCtrl", ["$scope",
 
 
 
-/**************************
+/*
  App Tables
- **************************/
+ Controller for dynamic and other tables
+ */
 
 angular.module("app.tables", []).controller("tableCtrl", ["$scope", "$filter",
     function($scope, $filter) {
@@ -1267,3 +1272,253 @@ angular.module("app.tables", []).controller("tableCtrl", ["$scope", "$filter",
         });
     }
 ]);
+
+/*
+ App Ui Controllers
+ Provides general controllers for the app
+ */
+
+angular.module("app.ui.ctrls", []).controller("NotifyCtrl", ["$scope", "loggit",
+    function($scope, loggit) {
+        $scope.notify = function(type) {
+            switch (type) {
+                case "info":
+                    return loggit.log("Hello! This is an alert of the info importance level.");
+                case "success":
+                    return loggit.logSuccess("Great! You did something successfully.");
+                case "warning":
+                    return loggit.logWarning("Warning! Something that happened that is not critical but important.");
+                case "error":
+                    return loggit.logError("Error! Something went terribly wrong and needs your attention.");
+            }
+        };
+    }
+]).controller("AlertDemoCtrl", ["$scope",
+    function($scope) {
+        $scope.alerts = [{
+            type: "success",
+            msg: "Great! You did something successfully."
+        }, {
+            type: "info",
+            msg: "Hello! This is an alert of the info importance level."
+        }, {
+            type: "warning",
+            msg: "Warning! Something that happened that is not critical but important."
+        }, {
+            type: "danger",
+            msg: "Error! Something went terribly wrong and needs your attention."
+        }];
+
+        $scope.addAlert = function() {
+            $scope.alerts.push({msg: 'Another alert!'});
+        };
+
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
+        };
+    }
+]).controller("ProgressDemoCtrl", ["$scope",
+    function($scope) {
+        $scope.max = 200;
+
+        $scope.random = function() {
+            var value = Math.floor((Math.random() * 100) + 1);
+            var type;
+
+            if (value < 25) {
+                type = 'success';
+            } else if (value < 50) {
+                type = 'info';
+            } else if (value < 75) {
+                type = 'warning';
+            } else {
+                type = 'danger';
+            }
+
+            $scope.showWarning = (type === 'danger' || type === 'warning');
+
+            $scope.dynamic = value;
+            $scope.type = type;
+        };
+        $scope.random();
+
+        $scope.randomStacked = function() {
+            $scope.stacked = [];
+            var types = ['success', 'info', 'warning', 'danger'];
+
+            for (var i = 0, n = Math.floor((Math.random() * 4) + 1); i < n; i++) {
+                var index = Math.floor((Math.random() * 4));
+                $scope.stacked.push({
+                    value: Math.floor((Math.random() * 30) + 1),
+                    type: types[index]
+                });
+            }
+        };
+        $scope.randomStacked();
+    }
+]).controller("AccordionDemoCtrl", ["$scope",
+    function($scope) {
+        return $scope.oneAtATime = !0, $scope.groups = [{
+            title: "First Group Header",
+            content: "First Group Body"
+        }, {
+            title: "Second Group Header",
+            content: "Second Group Body"
+        }, {
+            title: "Third Group Header",
+            content: "Third Group Body"
+        }], $scope.items = ["Item 1", "Item 2", "Item 3"], $scope.status = {
+            isFirstOpen: !0,
+            isFirstOpen1: !0,
+            isFirstOpen2: !0,
+            isFirstOpen3: !0,
+            isFirstOpen4: !0,
+            isFirstOpen5: !0,
+            isFirstOpen6: !0
+        }, $scope.addItem = function() {
+            var newItemNo;
+            newItemNo = $scope.items.length + 1;
+            $scope.items.push("Item " + newItemNo);
+        };
+    }
+]).controller("CollapseDemoCtrl", ["$scope",
+    function($scope) {
+        $scope.isCollapsed = !1;
+    }
+]).controller("ModalDemoCtrl", ["$scope", "$modal", "$log",
+    function($scope, $modal, $log) {
+        $scope.items = ['item1', 'item2', 'item3'];
+
+        $scope.open = function (size) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+    }
+]).controller("ModalInstanceCtrl", ["$scope", "$modalInstance", "items",
+    function($scope, $modalInstance, items) {
+        $scope.items = items;
+        $scope.selected = {
+            item: $scope.items[0]
+        };
+
+        $scope.ok = function () {
+            $modalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    }
+]).controller("PaginationDemoCtrl", ["$scope",
+    function($scope) {
+        $scope.totalItems = 64;
+        $scope.currentPage = 4;
+
+        $scope.setPage = function (pageNo) {
+            $scope.currentPage = pageNo;
+        };
+
+        $scope.pageChanged = function() {
+            console.log('Page changed to: ' + $scope.currentPage);
+        };
+
+        $scope.maxSize = 5;
+        $scope.bigTotalItems = 175;
+        $scope.bigCurrentPage = 1;
+    }
+]).controller("MapDemoCtrl", ["$scope", "$http", "$interval",
+    function($scope, $http, $interval) {
+        var i, markers;
+        for (markers = [], i = 0; 8 > i;){
+            markers[i] = new google.maps.Marker({
+                title: "Marker: " + i
+            });
+            i++;
+        }
+        $scope.GenerateMapMarkers = function() {
+            var d, lat, lng, loc, numMarkers;
+            for (d = new Date(), $scope.date = d.toLocaleString(), numMarkers = Math.floor(4 * Math.random()) + 4, i = 0; numMarkers > i;){
+                lat = 43.66 + Math.random() / 100;
+                lng = -79.4103 + Math.random() / 100;
+                loc = new google.maps.LatLng(lat, lng);
+                markers[i].setPosition(loc);
+                markers[i].setMap($scope.map);
+                i++;
+            }
+        }; $interval($scope.GenerateMapMarkers, 2e3);
+    }
+]).controller("TreeDemoCtrl", ["$scope",
+    function($scope) {
+        // Parameters
+
+        $scope.list = [{
+            "id": 1,
+            "title": "1. dragon-breath",
+            "items": []
+        }, {
+            "id": 2,
+            "title": "2. moiré-vision",
+            "items": [{
+                "id": 21,
+                "title": "2.1. tofu-animation",
+                "items": [{
+                    "id": 211,
+                    "title": "2.1.1. spooky-giraffe",
+                    "items": []
+                }, {
+                    "id": 212,
+                    "title": "2.1.2. bubble-burst",
+                    "items": []
+                }]
+            }, {
+                "id": 22,
+                "title": "2.2. barehand-atomsplitting",
+                "items": []
+            }]
+        }, {
+            "id": 3,
+            "title": "3. unicorn-zapper",
+            "items": []
+        }, {
+            "id": 4,
+            "title": "4. romantic-transclusion",
+            "items": []
+        }];
+
+        $scope.callbacks = {
+        };
+
+        $scope.remove = function(scope) {
+            scope.remove();
+        };
+
+        $scope.toggle = function(scope) {
+            scope.toggle();
+        };
+
+        $scope.newSubItem = function(scope) {
+            var nodeData = scope.$modelValue;
+            nodeData.items.push({
+                id: nodeData.id * 10 + nodeData.items.length,
+                title: nodeData.title + '.' + (nodeData.items.length + 1),
+                items: []
+            });
+        };
+    }
+]);
+
--- app/directives.js
+++ app/directives.js
@@ -1,8 +1,9 @@
 
 
-/**************************
- Chart directives
- **************************/
+/*
+ Charting directives
+ Provides custom directives for charting elements
+ */
 
 angular.module("app.chart.directives", []).directive("gaugeChart", [
         function() {
@@ -223,260 +224,10 @@ angular.module("app.chart.directives", []).directive("gaugeChart", [
     ]);
 
 
-
-/**************************
- App ui controllers
- **************************/
-
-angular.module("app.ui.ctrls", []).controller("NotifyCtrl", ["$scope", "loggit",
-        function($scope, loggit) {
-            $scope.notify = function(type) {
-                switch (type) {
-                    case "info":
-                        return loggit.log("Hello! This is an alert of the info importance level.");
-                    case "success":
-                        return loggit.logSuccess("Great! You did something successfully.");
-                    case "warning":
-                        return loggit.logWarning("Warning! Something that happened that is not critical but important.");
-                    case "error":
-                        return loggit.logError("Error! Something went terribly wrong and needs your attention.");
-                }
-            };
-        }
-    ]).controller("AlertDemoCtrl", ["$scope",
-        function($scope) {
-            $scope.alerts = [{
-                type: "success",
-                msg: "Great! You did something successfully."
-            }, {
-                type: "info",
-                msg: "Hello! This is an alert of the info importance level."
-            }, {
-                type: "warning",
-                msg: "Warning! Something that happened that is not critical but important."
-            }, {
-                type: "danger",
-                msg: "Error! Something went terribly wrong and needs your attention."
-            }];
-
-            $scope.addAlert = function() {
-                $scope.alerts.push({msg: 'Another alert!'});
-            };
-
-            $scope.closeAlert = function(index) {
-                $scope.alerts.splice(index, 1);
-            };
-        }
-    ]).controller("ProgressDemoCtrl", ["$scope",
-        function($scope) {
-            $scope.max = 200;
-
-            $scope.random = function() {
-                var value = Math.floor((Math.random() * 100) + 1);
-                var type;
-
-                if (value < 25) {
-                    type = 'success';
-                } else if (value < 50) {
-                    type = 'info';
-                } else if (value < 75) {
-                    type = 'warning';
-                } else {
-                    type = 'danger';
-                }
-
-                $scope.showWarning = (type === 'danger' || type === 'warning');
-
-                $scope.dynamic = value;
-                $scope.type = type;
-            };
-            $scope.random();
-
-            $scope.randomStacked = function() {
-                $scope.stacked = [];
-                var types = ['success', 'info', 'warning', 'danger'];
-
-                for (var i = 0, n = Math.floor((Math.random() * 4) + 1); i < n; i++) {
-                    var index = Math.floor((Math.random() * 4));
-                    $scope.stacked.push({
-                        value: Math.floor((Math.random() * 30) + 1),
-                        type: types[index]
-                    });
-                }
-            };
-            $scope.randomStacked();
-        }
-    ]).controller("AccordionDemoCtrl", ["$scope",
-        function($scope) {
-            return $scope.oneAtATime = !0, $scope.groups = [{
-                title: "First Group Header",
-                content: "First Group Body"
-            }, {
-                title: "Second Group Header",
-                content: "Second Group Body"
-            }, {
-                title: "Third Group Header",
-                content: "Third Group Body"
-            }], $scope.items = ["Item 1", "Item 2", "Item 3"], $scope.status = {
-                isFirstOpen: !0,
-                isFirstOpen1: !0,
-                isFirstOpen2: !0,
-                isFirstOpen3: !0,
-                isFirstOpen4: !0,
-                isFirstOpen5: !0,
-                isFirstOpen6: !0
-            }, $scope.addItem = function() {
-                var newItemNo;
-                newItemNo = $scope.items.length + 1;
-                $scope.items.push("Item " + newItemNo);
-            };
-        }
-    ]).controller("CollapseDemoCtrl", ["$scope",
-        function($scope) {
-            $scope.isCollapsed = !1;
-        }
-    ]).controller("ModalDemoCtrl", ["$scope", "$modal", "$log",
-        function($scope, $modal, $log) {
-            $scope.items = ['item1', 'item2', 'item3'];
-
-            $scope.open = function (size) {
-
-                var modalInstance = $modal.open({
-                    templateUrl: 'myModalContent.html',
-                    controller: 'ModalInstanceCtrl',
-                    size: size,
-                    resolve: {
-                        items: function () {
-                            return $scope.items;
-                        }
-                    }
-                });
-
-                modalInstance.result.then(function (selectedItem) {
-                    $scope.selected = selectedItem;
-                }, function () {
-                    $log.info('Modal dismissed at: ' + new Date());
-                });
-            };
-        }
-    ]).controller("ModalInstanceCtrl", ["$scope", "$modalInstance", "items",
-        function($scope, $modalInstance, items) {
-            $scope.items = items;
-            $scope.selected = {
-                item: $scope.items[0]
-            };
-
-            $scope.ok = function () {
-                $modalInstance.close($scope.selected.item);
-            };
-
-            $scope.cancel = function () {
-                $modalInstance.dismiss('cancel');
-            };
-        }
-    ]).controller("PaginationDemoCtrl", ["$scope",
-        function($scope) {
-            $scope.totalItems = 64;
-            $scope.currentPage = 4;
-
-            $scope.setPage = function (pageNo) {
-                $scope.currentPage = pageNo;
-            };
-
-            $scope.pageChanged = function() {
-                console.log('Page changed to: ' + $scope.currentPage);
-            };
-
-            $scope.maxSize = 5;
-            $scope.bigTotalItems = 175;
-            $scope.bigCurrentPage = 1;
-        }
-    ]).controller("MapDemoCtrl", ["$scope", "$http", "$interval",
-        function($scope, $http, $interval) {
-            var i, markers;
-            for (markers = [], i = 0; 8 > i;){
-                markers[i] = new google.maps.Marker({
-                    title: "Marker: " + i
-                });
-                i++;
-            }
-            $scope.GenerateMapMarkers = function() {
-                var d, lat, lng, loc, numMarkers;
-                for (d = new Date(), $scope.date = d.toLocaleString(), numMarkers = Math.floor(4 * Math.random()) + 4, i = 0; numMarkers > i;){
-                    lat = 43.66 + Math.random() / 100;
-                        lng = -79.4103 + Math.random() / 100;
-                        loc = new google.maps.LatLng(lat, lng);
-                        markers[i].setPosition(loc);
-                        markers[i].setMap($scope.map);
-                        i++;
-                }
-            }; $interval($scope.GenerateMapMarkers, 2e3);
-        }
-    ]).controller("TreeDemoCtrl", ["$scope",
-        function($scope) {
-            // Parameters
-
-            $scope.list = [{
-                "id": 1,
-                "title": "1. dragon-breath",
-                "items": []
-            }, {
-                "id": 2,
-                "title": "2. moiré-vision",
-                "items": [{
-                    "id": 21,
-                    "title": "2.1. tofu-animation",
-                    "items": [{
-                        "id": 211,
-                        "title": "2.1.1. spooky-giraffe",
-                        "items": []
-                    }, {
-                        "id": 212,
-                        "title": "2.1.2. bubble-burst",
-                        "items": []
-                    }],
-                }, {
-                    "id": 22,
-                    "title": "2.2. barehand-atomsplitting",
-                    "items": []
-                }],
-            }, {
-                "id": 3,
-                "title": "3. unicorn-zapper",
-                "items": []
-            }, {
-                "id": 4,
-                "title": "4. romantic-transclusion",
-                "items": []
-            }];
-
-            $scope.callbacks = {
-            };
-
-            $scope.remove = function(scope) {
-                scope.remove();
-            };
-
-            $scope.toggle = function(scope) {
-                scope.toggle();
-            };
-
-            $scope.newSubItem = function(scope) {
-                var nodeData = scope.$modelValue;
-                nodeData.items.push({
-                    id: nodeData.id * 10 + nodeData.items.length,
-                    title: nodeData.title + '.' + (nodeData.items.length + 1),
-                    items: []
-                });
-            };
-        }
-    ]);
-
-
-
-/**************************
+/*
  App custom Directives
- **************************/
+ Custom directives for the app like custom background, minNavigation etc
+ */
 
 angular.module("app.directives", []).directive("imgHolder", [
         function() {
@@ -543,6 +294,7 @@ angular.module("app.directives", []).directive("imgHolder", [
             return {
                 link: function(scope, ele) {
                     var $content, $nav, $window, Timer, app, updateClass;
+
                     return app = $("#app"), $window = $(window), $nav = $("#nav-container"), $content = $("#content"), ele.on("click", function(e) {
 
                         if(app.hasClass("nav-min")){
@@ -556,11 +308,15 @@ angular.module("app.directives", []).directive("imgHolder", [
 
                     }), Timer = void 0, updateClass = function() {
                         var width;
-                        return width = $window.width(), 768 > width ? app.removeClass("nav-min") : void 0;
+                        return width = $window.width(), 980 > width ? app.addClass("nav-min") : void 0;
+                    },initResize = function() {
+                        var width;
+                        return width = $window.width(), 980 > width ? app.addClass("nav-min") : app.removeClass("nav-min");
                     }, $window.resize(function() {
                         var t;
                         return clearTimeout(t), t = setTimeout(updateClass, 300);
-                    });
+                    }),initResize();
+
                 }
             };
         }
@@ -646,9 +402,10 @@ angular.module("app.directives", []).directive("imgHolder", [
 
 
 
-/**************************
+/*
  App Form Ui Directives
- **************************/
+ Custom directives for Form Ui elements
+ */
 
 angular.module("app.ui.form.directives", []).directive("uiRangeSlider", [
         function() {
